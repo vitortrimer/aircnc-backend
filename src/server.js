f5b10451e1748a868f9c054631de1b39f8a4c4f5
Vitor <vitor.trimer@ringa.com.br>
2019-10-06 16:52:09 -0300

    Add socket.io to backend

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
+const path = require("path");
+
+const socketio = require("socket.io");
+const http = require("http");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   "mongodb+srv://aircnc:omnistack@aircnc-yaipz.mongodb.net/aircnc?retryWrites=true&w=majority",
@@ -12,7 +19,23 @@ mongoose.connect(
   }
 );
 
+let connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
